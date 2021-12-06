4fd5c198e48aa74ef748e26729b842dab50623d9
zhaozeteng <zhaozeteng@126.com>
2021-12-06 17:29:50 +0800

    搭建项目环境

---
+++ src/main/java/com/inspur/human/HumanManageApplication.java
@@ -0,0 +1,12 @@
+package com.inspur.human;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumanManageApplication.class);
+    }
+}
